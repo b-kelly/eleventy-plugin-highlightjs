c92cff18a3878874416481db2084837e9c73d969
Aaron Shekey <aaron@aaronshekey.com>
2022-02-04 13:53:40 -0600

    Grab the upstream updates to LiquidHighlightTag.js
    We can deal with formatting opinions

--- src/LiquidHighlightTag.js
+++ src/LiquidHighlightTag.js
@@ -6,9 +6,9 @@ class LiquidHighlightTag {
     }
 
     getObject(options = {}) {
-        let ret = function (highlighter) {
+        let ret = function(highlighter) {
             return {
-                parse: function (tagToken, remainTokens) {
+                parse: function(tagToken, remainTokens) {
                     let split = tagToken.args.split(" ");
 
                     this.language = split.shift();
@@ -16,37 +16,29 @@ class LiquidHighlightTag {
 
                     this.tokens = [];
 
-                    var stream = highlighter.liquidEngine.parser.parseStream(
-                        remainTokens
-                    );
+                    var stream = highlighter.liquidEngine.parser.parseStream(remainTokens);
 
                     stream
-                        .on("token", (token) => {
+                        .on("token", token => {
                             if (token.name === "endhighlight") {
                                 stream.stop();
                             } else {
                                 this.tokens.push(token);
                             }
                         })
-                        .on("end", (x) => {
-                            throw new Error("tag highlight not closed");
+                        .on("end", x => {
+                            throw new Error(`tag ${tagToken.getText()} not closed`);
                         });
 
                     stream.start();
                 },
-                render: function (scope, hash) {
-                    let tokens = this.tokens.map((token) => token.raw);
+                render: function(scope, hash) {
+                    let tokens = this.tokens.map(token => {
+                        return token.raw || token.getText();
+                    });
                     let tokenStr = tokens.join("").trim();
-
-                    return Promise.resolve(
-                        HighlightPairedShortcode(
-                            tokenStr,
-                            this.language,
-                            this.highlightLines,
-                            options
-                        )
-                    );
-                },
+                    return Promise.resolve(HighlightPairedShortcode(tokenStr, this.language, this.highlightLines, options));
+                }
             };
         };
 
