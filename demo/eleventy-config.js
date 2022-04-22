cd70de016a29a38894d1d2204c2343be4174a09d
Ben Kelly <b-kelly@users.noreply.github.com>
2022-04-22 16:33:46 -0400

    chore: add test options to demo config

--- demo/eleventy-config.js
+++ demo/eleventy-config.js
@@ -3,6 +3,12 @@ const syntaxHighlight = require("../.eleventy.js");
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPlugin(syntaxHighlight, {
         className: "hljs",
+        preAttributes: {
+            tabindex: "0",
+        },
+        codeAttributes: {
+            "data-foo": "bar",
+        },
     });
 
     eleventyConfig.setTemplateFormats("njk,liquid,md,css");
